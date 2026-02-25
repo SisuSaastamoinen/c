3ccd52ca7cda61f6f95d33d82fc9db62687908d3
Sasti Saastamoinen <SisuSaastamoinen>
2026-02-25 15:00:42 +0200

    7

---
+++ projekt7/projekt7/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace projekt7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.LainaTB = new System.Windows.Forms.TextBox();
+            this.NesteetTB = new System.Windows.Forms.TextBox();
+            this.VakuutuksetTB = new System.Windows.Forms.TextBox();
+            this.MuutTB = new System.Windows.Forms.TextBox();
+            this.PolttoaineTB = new System.Windows.Forms.TextBox();
+            this.PesuTB = new System.Windows.Forms.TextBox();
+            this.HuollotTB = new System.Windows.Forms.TextBox();
+            this.RenkaatTB = new System.Windows.Forms.TextBox();
+            this.KilometritCB = new System.Windows.Forms.ComboBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auton kustannuslaskuri";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lainan lyhennys:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lisättävät nesteet:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Vakuutus maksut:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Muut kulut:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Polttoaine:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(319, 151);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 25);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Kilometrit:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(319, 117);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 25);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Renkaat:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(319, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 25);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Huollot:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(319, 49);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 25);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Pesut:";
+            // 
+            // LainaTB
+            // 
+            this.LainaTB.Location = new System.Drawing.Point(194, 46);
+            this.LainaTB.Name = "LainaTB";
+            this.LainaTB.Size = new System.Drawing.Size(100, 31);
+            this.LainaTB.TabIndex = 10;
+            // 
+            // NesteetTB
+            // 
+            this.NesteetTB.Location = new System.Drawing.Point(194, 80);
+            this.NesteetTB.Name = "NesteetTB";
+            this.NesteetTB.Size = new System.Drawing.Size(100, 31);
+            this.NesteetTB.TabIndex = 11;
+            // 
+            // VakuutuksetTB
+            // 
+            this.VakuutuksetTB.Location = new System.Drawing.Point(194, 114);
+            this.VakuutuksetTB.Name = "VakuutuksetTB";
+            this.VakuutuksetTB.Size = new System.Drawing.Size(100, 31);
+            this.VakuutuksetTB.TabIndex = 12;
+            // 
+            // MuutTB
+            // 
+            this.MuutTB.Location = new System.Drawing.Point(136, 148);
+            this.MuutTB.Name = "MuutTB";
+            this.MuutTB.Size = new System.Drawing.Size(100, 31);
+            this.MuutTB.TabIndex = 13;
+            // 
+            // PolttoaineTB
+            // 
+            this.PolttoaineTB.Location = new System.Drawing.Point(124, 183);
+            this.PolttoaineTB.Name = "PolttoaineTB";
+            this.PolttoaineTB.Size = new System.Drawing.Size(100, 31);
+            this.PolttoaineTB.TabIndex = 14;
+            // 
+            // PesuTB
+            // 
+            this.PesuTB.Location = new System.Drawing.Point(389, 46);
+            this.PesuTB.Name = "PesuTB";
+            this.PesuTB.Size = new System.Drawing.Size(100, 31);
+            this.PesuTB.TabIndex = 15;
+            // 
+            // HuollotTB
+            // 
+            this.HuollotTB.Location = new System.Drawing.Point(398, 80);
+            this.HuollotTB.Name = "HuollotTB";
+            this.HuollotTB.Size = new System.Drawing.Size(100, 31);
+            this.HuollotTB.TabIndex = 16;
+            // 
+            // RenkaatTB
+            // 
+            this.RenkaatTB.Location = new System.Drawing.Point(411, 114);
+            this.RenkaatTB.Name = "RenkaatTB";
+            this.RenkaatTB.Size = new System.Drawing.Size(100, 31);
+            this.RenkaatTB.TabIndex = 17;
+            // 
+            // KilometritCB
+            // 
+            this.KilometritCB.FormattingEnabled = true;
+            this.KilometritCB.Items.AddRange(new object[] {
+            "5000",
+            "10000",
+            "15000",
+            "20000",
+            "25000",
+            "30000",
+            "35000",
+            "40000",
+            "45000",
+            "50000",
+            "55000",
+            "60000",
+            "65000",
+            "70000",
+            "75000",
+            "80000",
+            "85000",
+            "90000",
+            "95000",
+            "100000"});
+            this.KilometritCB.Location = new System.Drawing.Point(421, 148);
+            this.KilometritCB.Name = "KilometritCB";
+            this.KilometritCB.Size = new System.Drawing.Size(121, 33);
+            this.KilometritCB.TabIndex = 18;
+            this.KilometritCB.SelectedIndexChanged += new System.EventHandler(this.KilometritCB_SelectedIndexChanged);
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.Location = new System.Drawing.Point(186, 267);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(240, 73);
+            this.VastausLB.TabIndex = 19;
+            this.VastausLB.Text = "label11";
+            this.VastausLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 442);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.KilometritCB);
+            this.Controls.Add(this.RenkaatTB);
+            this.Controls.Add(this.HuollotTB);
+            this.Controls.Add(this.PesuTB);
+            this.Controls.Add(this.PolttoaineTB);
+            this.Controls.Add(this.MuutTB);
+            this.Controls.Add(this.VakuutuksetTB);
+            this.Controls.Add(this.NesteetTB);
+            this.Controls.Add(this.LainaTB);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "KilometrikulutusLaskuri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox LainaTB;
+        private System.Windows.Forms.TextBox NesteetTB;
+        private System.Windows.Forms.TextBox VakuutuksetTB;
+        private System.Windows.Forms.TextBox MuutTB;
+        private System.Windows.Forms.TextBox PolttoaineTB;
+        private System.Windows.Forms.TextBox PesuTB;
+        private System.Windows.Forms.TextBox HuollotTB;
+        private System.Windows.Forms.TextBox RenkaatTB;
+        private System.Windows.Forms.ComboBox KilometritCB;
+        private System.Windows.Forms.Label VastausLB;
+    }
+}
+
---
+++ projekt7/projekt7/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projekt7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void KilometritCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double laina, nesteet, vakuutus, pesut, huollot, renkaat, muut, kilometrit, energia, kustannukset;
+            laina = Convert.ToDouble(LainaTB.Text);
+            nesteet = Convert.ToDouble(NesteetTB.Text);
+            vakuutus = Convert.ToDouble(NesteetTB.Text);
+            pesut = Convert.ToDouble(PesuTB.Text);
+            huollot = Convert.ToDouble(HuollotTB.Text);
+            renkaat = Convert.ToDouble(RenkaatTB.Text);
+            muut = Convert.ToDouble(MuutTB.Text);
+            kilometrit = Convert.ToDouble(KilometritCB.Text);
+            energia = Convert.ToDouble(PolttoaineTB.Text);
+            kustannukset = (laina + nesteet + vakuutus + pesut + huollot + renkaat + muut + kilometrit + energia) / (kilometrit / 12);
+            VastausLB.Text = "" + kustannukset;
+            VastausLB.Visible = true;
+        }
+    }
+}

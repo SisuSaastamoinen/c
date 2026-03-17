a2fc20a480321ebc1cadae4c968e409a5834f32f
Sasti Saastamoinen <SisuSaastamoinen>
2026-03-17 15:04:22 +0200

    .

---
+++ projekt13/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace projekt13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NimiTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.TarkastaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna nimi:";
+            // 
+            // NimiTB
+            // 
+            this.NimiTB.Location = new System.Drawing.Point(158, 39);
+            this.NimiTB.Margin = new System.Windows.Forms.Padding(6);
+            this.NimiTB.Name = "NimiTB";
+            this.NimiTB.Size = new System.Drawing.Size(218, 29);
+            this.NimiTB.TabIndex = 1;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.Location = new System.Drawing.Point(60, 103);
+            this.VastausLB.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(76, 24);
+            this.VastausLB.TabIndex = 2;
+            this.VastausLB.Text = "Vastaus";
+            // 
+            // TarkastaBT
+            // 
+            this.TarkastaBT.Location = new System.Drawing.Point(158, 94);
+            this.TarkastaBT.Name = "TarkastaBT";
+            this.TarkastaBT.Size = new System.Drawing.Size(75, 32);
+            this.TarkastaBT.TabIndex = 3;
+            this.TarkastaBT.Text = "Tarkasta";
+            this.TarkastaBT.UseVisualStyleBackColor = true;
+            this.TarkastaBT.Click += new System.EventHandler(this.TarkastaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 147);
+            this.Controls.Add(this.TarkastaBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.NimiTB);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NimiTB;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.Button TarkastaBT;
+    }
+}
+
---
+++ projekt13/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projekt13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TarkastaBT_Click(object sender, EventArgs e)
+        {
+            VastausLB.Text = "";
+            VastausLB.Visible = false;
+            string[] pojat = File.ReadAllLines("C: \\Users\\sasti\\source\\repos\\projekt1\\projekt13\\Pojat.txt");
+            string[] tytöt = File.ReadAllLines("C:\\Users\\sasti\\source\\repos\\projekt1\\projekt13\\Tytöt.txt");
+            string nimi = NimiTB.Text;
+            foreach(string poju in pojat)
+            {
+                if(nimi == poju)
+                {
+                    VastausLB.Text = "Nimi on suosittu";
+                    VastausLB.Visible = true;
+                }
+            }
+            foreach (string girls in tytöt)
+            {
+                if (nimi == girls)
+                {
+                    VastausLB.Text = "Nimi on suosittu";
+                    VastausLB.Visible = true;
+                }
+            }
+            if(NimiTB.Visible == false)
+            {
+                VastausLB.Text = "Nimi ei ole suosittu";
+                VastausLB.Visible = true;
+            }
+        }
+    }
+}
